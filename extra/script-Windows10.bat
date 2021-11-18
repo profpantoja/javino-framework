fe4d36debb21e7e6b836743a8429da64cdec5b38
Prof Nilson Mori <32855001+nilsonmori@users.noreply.github.com>
2021-11-18 17:38:39 -0300

    Script de Configuração - Windows 10

---
+++ extra/script-Windows10.bat
@@ -0,0 +1,130 @@
+cls
+@echo off
+echo Script de instalacao de ambiente do Javino para Windows
+echo.
+echo Este script realiza o download e instalacao do EclipseIDE, Java JDK-11 e Python 3
+echo.
+set /p choice="Deseja Continuar? (Digite [S] para confirmar):  "
+
+IF "%choice%"=="S" (
+	goto inicio
+) ELSE IF "%choice%"=="s" (
+	goto inicio
+) ELSE (
+	echo.
+	echo Instalacao Cancelada.
+	pause
+	exit
+)
+pause
+
+
+
+:inicio
+REM MD %HOMEDRIVE%\%HOMEPATH%\Javino
+
+cls
+echo Script de instalacao de ambiente do Javino para Windows
+echo.
+set /p choice="Deseja realizar o download e Instalacao do Eclipse IDE? (Digite [S] para confirmar):  "
+
+IF "%choice%"=="S" (
+	goto eclipse	
+) ELSE IF "%choice%"=="s" (
+	goto eclipse
+) ELSE (
+	goto ifjdk
+)
+
+:eclipse
+echo Iniciando o Download do Eclipse
+REM bitsadmin /transfer "Download" /download /priority FOREGROUND https://turing.pro.br/kadupantoja/t00ls/javino/W10/eclipse-inst-jre-win64.exe %homedrive%\%homepath%\Javino\eclipse.exe
+echo.
+timeout 2 > NUL
+echo Download concluido, iniciando o instalador. Siga as orientacoes do instalador.
+REM %homedrive%\%homepath%\Javino\eclipse.exe
+timeout 15 > NUL
+echo.
+pause
+
+:ifjdk
+cls
+echo Script de instalacao de ambiente do Javino para Windows
+echo.
+set /p choice="Deseja realizar o download e Instalacao do Java JDK 11? (Digite [S] para confirmar):  "
+IF "%choice%"=="S" (
+	goto jdk
+) ELSE IF "%choice%"=="s" (
+	goto jdk
+) ELSE (
+	goto ifpython
+)
+
+:jdk
+echo Iniciando o Download do Java JDK 11
+REM bitsadmin /transfer "Download" /download /priority FOREGROUND https://turing.pro.br/kadupantoja/t00ls/javino/W10/jdk-11.0.12_windows-x64_bin.exe %homedrive%\%homepath%\Javino\jdk11.exe
+echo.
+timeout 2 > NUL
+echo Download concluido, iniciando o instalador. Siga as orientacoes do instalador.
+REM %homedrive%\%homepath%\Javino\jdk11.exe
+timeout 15 > NUL
+echo.
+pause
+
+IF NOT EXIST "%PROGRAMFILES%\Java\jdk-11.0.12\bin\javac.exe" goto varerro
+
+echo Adicionando Variavel de Ambiente JAVA_HOME
+REM SETX /M PATH "%PATH%;%PROGRAMFILES%\Java\jdk-11.0.12\bin"
+REM SETX /M JAVA_HOME "%PROGRAMFILES%\Java\jdk-11.0.12"
+goto varok
+
+:varerro
+echo.
+echo ATENCAO: Configure MANUALMENTE as variaveis de ambiente do JAVA (JAVA_HOME e PATH)
+echo Nao foi encontrado o diretorio %PROGRAMFILES%\Java\jdk-11.0.12
+pause
+
+:varok
+echo.
+pause
+
+
+
+:ifpython
+cls
+echo Script de instalacao de ambiente do Javino para Windows
+echo.
+set /p choice="Deseja realizar o download e Instalacao do Python 3? (Digite [S] para confirmar):  "
+IF "%choice%"=="S" (
+	goto python
+) ELSE IF "%choice%"=="s" (
+	goto python
+) ELSE (
+	goto fim
+)
+
+:python
+echo Iniciando o Download do Python 3
+REM bitsadmin /transfer "Download" /download /priority FOREGROUND https://turing.pro.br/kadupantoja/t00ls/javino/W10/python-3.10.0-amd64.exe %homedrive%\%homepath%\Javino\python3.exe
+echo.
+timeout 2 > NUL
+echo Download concluido, iniciando o instalador. Siga as orientacoes do instalador.
+REM %homedrive%\%homepath%\Javino\python3.exe
+timeout 15 > NUL
+echo.
+pause
+
+:fim
+cls
+echo Script de instalacao de ambiente do Javino para Windows
+echo.
+echo Fim!
+echo.
+pause
+exit
+
+
+
+
+
+
